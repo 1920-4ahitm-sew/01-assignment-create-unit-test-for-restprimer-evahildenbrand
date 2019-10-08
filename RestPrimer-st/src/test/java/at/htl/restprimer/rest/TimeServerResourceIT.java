e8f79865f15435aefc74ce1117f65041ca502a2a
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-08 19:56:50 +0200

    Added method fetchTime

--- RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
+++ RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -1,10 +1,17 @@
 package at.htl.restprimer.rest;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
 
 public class TimeServerResourceIT {
     private Client client;
@@ -14,5 +21,14 @@ public class TimeServerResourceIT {
     public void initClient(){
         this.client = ClientBuilder.newClient();
         this.target = this.client.target("http://localhost:8080/restprimer/api/time");
+    }
+
+    @Test
+    public void fetchTime(){
+        Response response = this.target.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(),is(200));
+
+        String payload = response.readEntity(String.class);
+        assertTrue(payload.startsWith("Time: "));
     }
 }
