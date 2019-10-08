0b313d339372c6b05bd276f2f77adba341ecf8f1
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-08 19:49:04 +0200

    Added method initClient

--- RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
+++ RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -1,4 +1,18 @@
 package at.htl.restprimer.rest;
 
+import org.junit.Before;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 public class TimeServerResourceIT {
+    private Client client;
+    private WebTarget target;
+
+    @Before
+    public void initClient(){
+        this.client = ClientBuilder.newClient();
+        this.target = this.client.target("http://localhost:8080/restprimer/api/time");
+    }
 }
