13a7d816f20532eb69730117e566c64e81ded54e
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-08 20:52:24 +0200

    Added comments for RestPrimer

--- RestPrimer/src/main/java/at/htl/restprimer/rest/RestConfig.java
+++ RestPrimer/src/main/java/at/htl/restprimer/rest/RestConfig.java
@@ -1,7 +1,9 @@
 package at.htl.restprimer.rest;
 
+//region Imports
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+//endregion
 
 @ApplicationPath("api")
 public class RestConfig extends Application {
--- RestPrimer/src/main/java/at/htl/restprimer/rest/TimeServerResource.java
+++ RestPrimer/src/main/java/at/htl/restprimer/rest/TimeServerResource.java
@@ -1,13 +1,20 @@
 package at.htl.restprimer.rest;
 
+//region Imports
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+//endregion
 
 @Path("time")
 public class TimeServerResource {
 
+    /**
+     * Method to show the current date and time
+     *
+     * @return date and time in format dd. MMMM yyyy, hh:mm:ss
+     */
     @GET
     public String time(){
         return "Time: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd. MMMM yyyy, hh:mm:ss"));
