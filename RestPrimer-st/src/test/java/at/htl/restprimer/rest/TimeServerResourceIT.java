9baac7f6e1d395472929f47348f49abf312ca1f9
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-08 20:48:29 +0200

    Added comments

--- RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
+++ RestPrimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -1,5 +1,6 @@
 package at.htl.restprimer.rest;
 
+//region Imports
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,24 +13,33 @@ import javax.ws.rs.core.Response;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertTrue;
+//endregion
 
 public class TimeServerResourceIT {
+    //Variablen
     private Client client;
     private WebTarget target;
 
     @Before
     public void initClient(){
+        //Creates new Client
         this.client = ClientBuilder.newClient();
+        //Sets the target to the Wildfly-Server of the RestPrimer
         this.target = this.client.target("http://localhost:8080/restprimer/api/time");
     }
 
     @Test
     public void fetchTime(){
+        //Sending Request to get plain-Text from the target and saving the Response
         Response response = this.target.request(MediaType.TEXT_PLAIN).get();
+        //Checks if the response has the status 200, so that the request was successful
         assertThat(response.getStatus(),is(200));
 
+        //Reads the message input string
         String payload = response.readEntity(String.class);
+        //Checks if the payload starts with the String "Time: "
         assertTrue(payload.startsWith("Time: "));
+        
         System.out.println("payload = " + payload);
     }
 }
